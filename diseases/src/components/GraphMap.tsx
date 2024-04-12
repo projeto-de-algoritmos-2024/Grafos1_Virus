dc36d205793deb0bce7be6f10db8b7b355d4a49d
Gustavo <gustavohenriqueprivado@gmail.com>
2024-04-11 23:03:30 -0300

    corrigindo grafo Co-Authored-by: Gustavo Kenzo <gustavokenzo314@gmail.com>

--- diseases/src/components/GraphMap.tsx
+++ diseases/src/components/GraphMap.tsx
@@ -1,92 +1,102 @@
-import { useState, useCallback, useEffect, useRef, useMemo } from 'react';
-import { ForceGraph3D } from 'react-force-graph';
+import { ForceGraph3D } from "react-force-graph";
+import { getRandomAdjacencyList } from "../utils/seed";
 import {
   CSS2DObject,
   CSS2DRenderer,
-} from 'three/addons/renderers/CSS2DRenderer.js';
+} from "three/examples/jsm/renderers/CSS2DRenderer.js";
+import { useCallback } from "react";
+import { Graph, Pessoa } from "../types/GraphTypes";
+import { useState } from "react";
 
-import { genRandomSeeds } from '../utils/seed';
-import { Graph, Link, Pessoa } from '../types/GraphTypes';
-
-interface IGraphMapProps {
-  isAddingPerson: boolean;
-  setIsAddingPerson: (isAddingPerson: boolean) => void;
-  personName: string;
-  setPersonName: (personName: string) => void;
-}
-
-export function GraphMap({}: IGraphMapProps) {
-  const pessoasSeed = genRandomSeeds();
+export function GraphMap() {
+  const pessoasSeed = getRandomAdjacencyList();
   const extraRenderers = [new CSS2DRenderer() as any];
-  const fgRef = useRef();
-
   const [pessoas, setPessoas] = useState<Graph>(pessoasSeed);
+
+  function getAdjacentNodes(
+    adjacencyList: Record<number, { target: number; value: number }[]>,
+    targetNodeId: number
+  ): number[] {
+    const adjacentNodes: number[] = [];
+    for (const [nodeId, connections] of Object.entries(adjacencyList)) {
+      const hasTarget = connections.some(
+        (connection) => connection.target === targetNodeId
+      );
+      if (hasTarget) {
+        adjacentNodes.push(parseInt(nodeId));
+      }
+
+      if (parseInt(nodeId) === targetNodeId) {
+        connections.forEach((connection) => {
+          adjacentNodes.push(connection.target);
+        });
+      }
+    }
+    return adjacentNodes;
+  }
+
+  function BFS(nodeId: number) {
+    let queue: number[] = [nodeId];
+    let newNodes = [...pessoas.nodes];
+    newNodes[nodeId].isInfected = true;
+
+    const visitNode = () => {
+      if (queue.length === 0) {
+        return;
+      }
+
+      const current = queue.shift();
+      const adjNodes = getAdjacentNodes(
+        pessoas.adjacencyList,
+        current as number
+      );
+
+      adjNodes.forEach((adjNodeId) => {
+        if (!newNodes[adjNodeId].isInfected) {
+          newNodes[adjNodeId].isInfected = true;
+          queue.push(adjNodeId);
+        }
+      });
+
+      newNodes.forEach((n) => {
+        n.fx = n.x;
+        n.fy = n.y;
+        n.fz = n.z;
+      });
+
+      setPessoas((prevPessoas) => ({
+        ...prevPessoas,
+        nodes: newNodes,
+      }));
+
+      setTimeout(visitNode, 1000); // Delay the next visit by 1 second
+    };
+
+    visitNode();
+  }
 
   const handleClick = useCallback(
     (node: Pessoa) => {
-      const { nodes, links } = pessoas;
-
-      const newNodes = nodes.slice().map((n) => {
-        if (n.id === node.id) {
-          n.isInfected = !n.isInfected;
-        }
-
-        return n;
-      });
-
-      newNodes.forEach((n, idx) => {
-        n.id = idx;
-      });
-
-      setPessoas({ nodes: newNodes, links: links });
+      BFS(node.id);
     },
-    [pessoas, setPessoas]
+    [pessoas]
   );
 
-  // useEffect(() => {
-  //   setInterval(() => {
-  //     const { nodes, links } = pessoas;
-  //     const infectedNodes = nodes.filter((n) => n.isInfected);
-
-  //     links.map((l) => {
-  //       const source = l.source.id;
-  //       const target = l.target.id;
-
-  //       if (infectedNodes.some((n) => n.id === source)) {
-  //         nodes[target].isInfected = true;
-  //       }
-
-  //       return l;
-  //     });
-
-  //     setPessoas({ nodes: nodes, links: links });
-  //   }, 3000);
-  // }, []);
   return (
     <ForceGraph3D
-      ref={fgRef}
-      enableNodeDrag={false}
-      // onNodeClick={(node) => (isAddingPerson ? addPerson(node) : handleClick)}
-      onNodeClick={handleClick}
       graphData={pessoas}
-      linkWidth={1.5}
-      nodeVal={(node) => (node.isInfected ? 10 : 5)}
-      nodeColor={(node) => (node.isInfected ? 'red' : '#63affb')}
-      backgroundColor="#fafafa"
-      linkColor={(link: any) => {
-        return link.source.isInfected && link.target.isInfected
-          ? 'red'
-          : '#333';
-      }}
-      nodeLabel={(node) => node.name}
+      onNodeClick={handleClick}
+      nodeVal={15}
+      nodeColor={(node) => (node.isInfected ? "red" : "blue")}
+      nodeLabel={(node) => `${node.name} - ${node.id}`}
       nodeOpacity={0.9}
       nodeThreeObject={(node) => {
-        const nodeEl = document.createElement('div');
-        nodeEl.textContent = node.name;
-        nodeEl.style.color = '#333';
-        nodeEl.style.fontSize = '8px';
-        nodeEl.style.fontWeight = '600';
-        nodeEl.className = 'node-label';
+        const nodeEl = document.createElement("div");
+        nodeEl.textContent = `${node.name} - ${node.id}`;
+        nodeEl.style.color = "#fff";
+        nodeEl.style.fontSize = "12px";
+        nodeEl.style.fontWeight = "600";
+        nodeEl.className = "node-label";
         return new CSS2DObject(nodeEl);
       }}
       nodeThreeObjectExtend={true}
