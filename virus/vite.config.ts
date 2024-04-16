385abd93bb192a09e089d95769687647ae5929a0
Gustavo Kenzo Araki Takechi <gustavokenzo314@gmail.com>
2024-04-15 21:37:40 -0300

    aaaaaa

--- virus/vite.config.ts
+++ virus/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
